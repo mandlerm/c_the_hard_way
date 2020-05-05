2ba4567543e04b2ff0e4656008849c1f2c14047f
mandlerm <amaya.melissa@gmail.com>
2020-05-04 20:49:16 -0500

    ex12

--- ex11-15/ex11.c
+++ ex11-15/ex11.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char* argv[]) {
 
@@ -6,23 +7,25 @@ int main(int argc, char* argv[]) {
   char name[4]= {'m'} ;
 
 
-  printf("numbers: %d %d %d %d\n", numbers[0], numbers[1], numbers[2], numbers[3]) ;
-  printf("name each: %c %c %c %c\n", name[0], name[1], name[2], name[3]);
-  printf("name: %s\n", name);
+  // printf("numbers: %d %d %d %d\n", numbers[0], numbers[1], numbers[2], numbers[3]) ;
+  // printf("name each: %c %c %c %c\n", name[0], name[1], name[2], name[3]);
+  // printf("name: %s\n", name);
 
 
   numbers[0] = 1;
-  numbers[1] = 'a';
-  numbers[2] = 3;
-  numbers[3] = 4;
+  numbers[1] = 234542656;
+  numbers[2] = 3092938;
+  numbers[3] = 423432;
 
   name[0] = 'Z';
   name[1] = 'e';
-  name[2] = 'd';
+  name[2] = ' d';
   name[3] = '\0';
 
-
-  // memcpy(name, numbers, 4);
+  int cpy[4] ;
+  printf("nums is: %d %d %d %d\n", numbers[0], numbers[1], numbers[2], numbers[3]);
+  memcpy(cpy, numbers, 6);
+  printf("cpy is: %d %d %d %d\n", cpy[0], cpy[1], cpy[2], cpy[3]);
   // printf("memCpy %s\n", name+2);
   //
   // printf("numbers: %d %d %d %d\n", numbers[0], numbers[1], numbers[2], numbers[3]);
@@ -32,7 +35,7 @@ int main(int argc, char* argv[]) {
 
   char *another = name;
 
-  printf("another each: %c %c %c %c\n", another[0], another[1], another[2], another[3]);
+  // printf("another each: %c %c %c %c\n", another[0], another[1], another[2], another[3]);
 
   return 0;
 }
